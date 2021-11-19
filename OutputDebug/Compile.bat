ff32a181564107b400b94c1158229df833e81e3d
dinghaoxiang <michael19950415@outlook.com>
2021-11-19 18:05:42 +0800

    Update Compile.bat

--- OutputDebug/Compile.bat
+++ OutputDebug/Compile.bat
@@ -1,13 +1,15 @@
 @echo off
 cd "..\"
-set Buildx64="..\OutputDebug\Publish\x64"
-set BuildArm="..\OutputDebug\Publish\arm64"
-set Libsx64="..\OutputDebug\Publish\x64\Libs"
-set LibsArm64="..\OutputDebug\Publish\arm64\Libs"
+set Buildx64=%cd%\OutputDebug\Publish\x64
+set BuildArm=%cd%\OutputDebug\Publish\arm64
+set Libsx64=%cd%\OutputDebug\Publish\x64\Libs
+set LibsArm64=%cd%\OutputDebug\Publish\arm64\Libs
 if exist %Buildx64%\ (del /q/a/f/s %Buildx64%\*.*) 
 if exist %BuildArm%\ (del /q/a/f/s %BuildArm%\*.*) 
 dotnet publish -c Release -r win-x64 -o OutputDebug/Publish/x64 --self-contained true
 dotnet publish -c Release -r win-arm64 -o OutputDebug/Publish/arm64 --self-contained true
-xcopy %Libsx64% %Buildx64% /q /e /r /s /y
-xcopy %LibsArm64% %BuildArm% /q /e /r /s /y
+xcopy %Libsx64% %Buildx64% /e /s /y /i
+xcopy %LibsArm64% %BuildArm% /e /s /y /i
+rd /q/s %Libsx64%
+rd /q/s %LibsArm64%
 pause
